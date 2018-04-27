7e34378703d1e72bd6f984387d8106dfb12465bf
gchauhan <gchauhan@ITEM-S56330.emea.msad.sopra>
2018-04-27 18:37:20 +0530

    Post

--- SpringTestProj/src/main/java/com/example/demo/controller/SimpleController.java
+++ SpringTestProj/src/main/java/com/example/demo/controller/SimpleController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +9,12 @@ public class SimpleController {
 	
 	@RequestMapping("/hello")
 	public String display(){
+		
+		return "Controller";
+	}
+	
+	@RequestMapping(value = "/hello2", method = { RequestMethod.POST  }, headers = {"Content-type=application/json"})
+	public String display2 (){
 		
 		return "Controller";
 	}
