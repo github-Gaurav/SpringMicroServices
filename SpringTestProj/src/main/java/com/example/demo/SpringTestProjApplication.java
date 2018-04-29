05f9a961926ad2035b6eab0c9257a5200c81b957
gaura <gaura@DESKTOP-DBJOEC5>
2018-04-30 01:40:32 +0530

    Push

--- SpringTestProj/src/main/java/com/example/demo/SpringTestProjApplication.java
+++ SpringTestProj/src/main/java/com/example/demo/SpringTestProjApplication.java
@@ -9,6 +9,9 @@ public class SpringTestProjApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Test");
+		
+		
+		
 		SpringApplication.run(SpringTestProjApplication.class, args);
 		
 	}
